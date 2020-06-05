10213b71471d3a4be4865bba200bd32e47900936
Philipp Seeger <philippbseeger@googlemail.com>
2020-06-05 19:00:19 +0200

    trying to get negative harmony test gtest fixture to run

--- Test/NegativeHarmonyTest.cpp
+++ Test/NegativeHarmonyTest.cpp
@@ -2,10 +2,23 @@
 // Created by phili on 05/06/2020.
 //
 
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+#include "../Source/MidiProcessor.h"
 
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+class MidiProcessorFixture : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    midi_processor_ = new MidiProcessor();
+  }
+  virtual void TearDown() {
+    delete midi_processor_;
+  }
+
+  MidiProcessor *midi_processor_;
+};
+
+TEST_F(MidiProcessorFixture, SomeTest) {
+  int negHarmNn = midi_processor_->getNegHarmNn(0, 0);
+
+  EXPECT_EQ(negHarmNn, 7);
 }
